897043d4f52efe75b75b19963892956d3eb5679b
gta multiverse rp <gta-multiverse-rp@fx.server>
2025-03-03 17:52:59 +0900

    導入

--- client.lua
+++ client.lua
@@ -35,9 +35,10 @@ function setSpeed(vehicle)
 	local vehicleClass = GetVehicleClass(vehicle)
 
 	if (vehicleClass ~= 16) or (vehicleClass ~= 15) then
-		local speed = Config.maxSpeed;
+		local speed = Config.maxSpeed
 		if Config.useCategories then
-			speed = Config.Categories[vehicleClass + 1].maxSpeed;
+			speed = Config.Categories[vehicleClass + 1].maxSpeed
+--			speed = Config.Categories[vehicleClass].maxSpeed
 		end
 		if Config.kmh then
 			speed = speed / Config.kmhValue
@@ -48,4 +49,35 @@ function setSpeed(vehicle)
 		SetVehicleMaxSpeed(vehicle, speed)
 		isSpeedSet = true
 	end
+end
+
+-- Anti VDM --
+if Config.antivdm then
+	Citizen.CreateThread(function()
+		while true do
+			SetWeaponDamageModifier(-1553120962, 0.0)
+			Citizen.Wait(10)
+		end
+	end)
+end
+	
+	-- Disable Vehicle Weapons --
+if Config.disablevehicleweapons then
+	Citizen.CreateThread(function()
+		while true do
+		local wait = 500
+			local playerped = PlayerPedId()
+			if IsPedInAnyVehicle(playerped, false) then
+				wait = 0
+				local veh = GetVehiclePedIsUsing(playerped)
+				if DoesVehicleHaveWeapons(veh) == 1 and vehicleweaponhash ~= 1422046295 then
+					vehicleweapon, vehicleweaponhash = GetCurrentPedVehicleWeapon(playerped)
+					if vehicleweapon == 1 then
+						DisableVehicleWeapon(true, vehicleweaponhash, veh, playerped)
+					end
+				end
+			end
+			Citizen.Wait(wait)
+		end
+	end)
 end
--- config.lua
+++ config.lua
@@ -4,40 +4,40 @@ Config.kmh = true
 
 -- Set Config.maxSpeed to false if you want to use same speed for all vehicles
 -- Set the max speed for all vehicles on Config.maxSpeed
-Config.useCategories = false
-Config.maxSpeed = 150
+Config.useCategories = true
+Config.maxSpeed = 320
 
 Config.Categories = {
    -- COMPACTS
-   {category = 0, maxSpeed = 100},
+   {category = 0, maxSpeed = 300},
    -- SEDANS
-   {category = 1, maxSpeed = 100},
+   {category = 1, maxSpeed = 300},
    -- SUV'S
-   {category = 2, maxSpeed = 100},
+   {category = 2, maxSpeed = 300},
    -- COUPES
-   {category = 3, maxSpeed = 100},
+   {category = 3, maxSpeed = 300},
    -- MUSCLE
-   {category = 4, maxSpeed = 100},
+   {category = 4, maxSpeed = 300},
    -- SPORT CLASSIC
-   {category = 5, maxSpeed = 100},
+   {category = 5, maxSpeed = 300},
    -- SPORT
-   {category = 6, maxSpeed = 100},
+   {category = 6, maxSpeed = 300},
    -- SUPER
-   {category = 7, maxSpeed = 100},
+   {category = 7, maxSpeed = 300},
    -- MOTORCYCLES
-   {category = 8, maxSpeed = 100},
+   {category = 8, maxSpeed = 300},
    -- OFFROAD
-   {category = 9, maxSpeed = 100},
+   {category = 9, maxSpeed = 300},
    -- INDUSTRIAL
-   {category = 10, maxSpeed = 100},
+   {category = 10, maxSpeed = 300},
    -- UTILITY
-   {category = 11, maxSpeed = 100},
+   {category = 11, maxSpeed = 300},
    -- VANS
-   {category = 12, maxSpeed = 100},
+   {category = 12, maxSpeed = 300},
    -- BICYCLES
-   {category = 13, maxSpeed = 100},
+   {category = 13, maxSpeed = 300},
    -- BOATS
-   {category = 14, maxSpeed = 100},
+   {category = 14, maxSpeed = 300},
 
    --#region PLANES AND HELIS
    -- YOU MUST NOT LOCK THIS ONES OR YOU WON'T BE ABLE TO FLY THEM!!!
@@ -46,13 +46,22 @@ Config.Categories = {
    --#endregion
 
    -- SERVICE
-   {category = 17, maxSpeed = 100},
+   {category = 17, maxSpeed = 300},
    -- EMERGENCY
-   {category = 18, maxSpeed = 100},
+   {category = 18, maxSpeed = 320},
    -- MILITARY
-   {category = 19, maxSpeed = 100}
+   {category = 19, maxSpeed = 300},
+   -- Open Wheel
+   {category = 22, maxSpeed = 300},
 }
 
 -- DO NOT MODIFY
 Config.kmhValue = 3.6
 Config.mphValue = 2.23694
+
+-- Anti VDM --
+Config.antivdm = true -- Makes running people over with your vehicle do 0 damage
+
+
+-- Disable Vehicle Weapons --
+Config.disablevehicleweapons = false -- Disables all vehicle weapons
